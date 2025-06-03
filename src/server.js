b5b779293629d005cf5725e1fac2c64e6fbca239
diegourday <dh7586@outlook.com>
2025-06-03 12:06:19 -0500

    cron.js added

--- src/server.js
+++ src/server.js
@@ -4,10 +4,13 @@ import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 import transactionsRoute from "./routes/transactionsRoute.js";
+import job from "./config/cron.js";
 
 dotenv.config();
 
 const app = express();
+
+if (process.env.NODE_ENV === "production") job.start();
 
 // middleware
 app.use(rateLimiter);
@@ -20,6 +23,12 @@ app.use(express.json());
 // });
 
 const PORT = process.env.PORT || 5001;
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+  });
+});
 
 app.use("/api/transactions", transactionsRoute);
 
